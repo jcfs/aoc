15fe9e2ca0f6c03b1ab020cb3d842bca7aabeaf4
jcfs <joao.salavisa@gmail.com>
2015-12-08 16:02:34 +0000

    indent

--- 8/p2.c
+++ 8/p2.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 int main(int argc, char ** argv) {
-    char line[128];
-		int i = 0;	
-		int count = 0;
-	
-    while(scanf("%s\n", line) != -1) {
-			for(i = 0; i< strlen(line); i++) 
-				if (line[i] == '\\' || line[i] == '\"') count++;
-			count += 2;
-		}   
-		printf("%d", count);
+	char line[128];
+	int i = 0;	
+	int count = 0;
+
+	while(scanf("%s\n", line) != -1) {
+		for(i = 0; i< strlen(line); i++) 
+			if (line[i] == '\\' || line[i] == '\"') count++;
+		count += 2;
+	}   
+	printf("%d", count);
 }
