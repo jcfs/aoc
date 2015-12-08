804d7a6ca97944fd1cbe2431bef23f3dcd36996d
jcfs <joao.salavisa@gmail.com>
2015-12-08 14:46:01 +0000

    clean up

--- 8/p1.c
+++ 8/p1.c
@@ -2,14 +2,13 @@
 #include <string.h>
 
 int main(int argc, char ** argv) {
-    char line[128];
-		int result = 0;
-		printf("int main(int argc, char ** argv) {\n int result = 0; \n");
+	char line[128];
+	int result = 0;
+	printf("int main(int argc, char ** argv) {\n int result = 0; \n");
 		
-    while(scanf("%s\n", line) != -1) {
-			result += strlen(line);
-			printf(" result += printf(%s);\n",line);
-		}   
-		printf("printf(\"%%d\\n\", %d- result);\n}", result);
-    
+	while(scanf("%s\n", line) != -1) {
+		result += strlen(line);
+		printf(" result += printf(%s);\n",line);
+	}   
+	printf("printf(\"%%d\\n\", %d- result);\n}", result);  
 }
