86c9f5915e7cb3d8ef66ee8c86cb1c5df41becf2
jcfs <joao.salavisa@gmail.com>
2015-12-08 14:26:21 +0000

    Day 8

---
+++ 8/p1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include <regex.h>
+
+
+
+
+int main(int argc, char ** argv) {
+    char line[128];
+		int result = 0;
+		printf("int main(int argc, char ** argv) {\n int result = 0; \n");
+		
+    while(scanf("%s\n", line) != -1) {
+			result += strlen(line);
+			printf(" result += printf(%s);\n",line);
+		}   
+		printf("printf(\"%%d\\n\", %d- result);\n}", result);
+    
+}
---
+++ 8/p2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char ** argv) {
+    char line[128];
+		int i = 0;	
+		int count = 0;
+	
+    while(scanf("%s\n", line) != -1) {
+			for(i = 0; i< strlen(line); i++) 
+				if (line[i] == '\\' || line[i] == '\"') count++;
+			count += 2;
+		}   
+		printf("%d", count);
+}
