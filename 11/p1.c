a433124c84fefe7fa8ff50d63e67a89972820665
jcfs <joao.salavisa@gmail.com>
2015-12-11 17:41:39 +0000

    indent

--- 11/p1.c
+++ 11/p1.c
@@ -13,15 +13,15 @@ void increase(char * str) {
 
 
 int main(int argc, char ** argv) {
-	regex_t regex[3];
-	char str[9];
-	int i = 0;
-	
+    regex_t regex[3];
+    char str[9];
+    int i = 0;
+
     sprintf(str, "%s\0", "ixbxwxba");
 
-	regcomp(&regex[0], ".*(abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz).*", REG_EXTENDED);
-	regcomp(&regex[1], ".*[ilo].*", REG_EXTENDED);
-	regcomp(&regex[2], ".*([a-z])\\1.*([a-z])\\2.*", REG_EXTENDED);
+    regcomp(&regex[0], ".*(abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz).*", REG_EXTENDED);
+    regcomp(&regex[1], ".*[ilo].*", REG_EXTENDED);
+    regcomp(&regex[2], ".*([a-z])\\1.*([a-z])\\2.*", REG_EXTENDED);
 
     for(i = 0; ; i++) {
         if (!regexec(&regex[0], str, 0, NULL, 0) && regexec(&regex[1], str, 0, NULL, 0) && !regexec(&regex[2], str, 0, NULL, 0)) break;
