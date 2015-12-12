6cc30debe1ccaa3d7919fe36cf0cc388e061816b
jcfs <joao.salavisa@gmail.com>
2015-12-12 15:02:53 +0000

    pythonic python

--- 12/p2.py
+++ 12/p2.py
@@ -3,18 +3,15 @@ import sys
 
 def getSum(js):
   if isinstance(js, list):
-    return sum([getSum(value) for value in js])
+    return sum([getSum(key) for key in js])
   elif isinstance(js, int):
     return js
   elif isinstance(js, basestring):
     return 0
-  elif 'red' in js.values():
-    return 0
   else:
-    return sum([getSum(js[key]) for key in js])
+    return sum([getSum(js[key]) for key in js if not 'red' in js.values()])
 
 inp = sys.stdin.readline()
 js = json.loads(inp)
 
-sum = getSum(js)
-print sum
+print getSum(js)
