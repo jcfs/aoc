140038dbf86fcb281041930aab29ea43003b4eea
jcfs <joao.salavisa@gmail.com>
2015-12-28 14:46:50 +0000

    less one var

--- 25/p1.c
+++ 25/p1.c
@@ -3,17 +3,16 @@
 
 int main(int argc, char ** argv) {
 
-  int l = 2, i, j;
+  int i, j = 2;
   long long p = 20151125;
 
   while (1) {
-    for (i = l, j = 1; i > 0; i--, j++) {
+    for (i = j, j = 1; i > 0; i--, j++) {
       p = p * 252533 % 33554393;
       if (i == 2947 && j == 3029) {
         printf("%lld\n", p);
         exit(0);
       }
     }
-    l++;
   }
 }
