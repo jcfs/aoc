e35ba013174be4cf2c48e792edb697659957bdee
Joao Salavisa <joao.salavisa@gmail.com>
2016-12-01 19:51:59 +0000

    refactor tree

--- 2016/1/p2.c
+++ 2016/1/p2.c
@@ -2,74 +2,78 @@
 #include <math.h>
 #include <stdlib.h>
 
+// aux shit.... ty c
+#define MIN(x, y) (x < y)? x : y
+#define MAX(x, y) (x > y)? x : y
+
+#define CITY_SIZE 1000
+#define START_POS 500
+
 #define NORTH 0
 #define EAST 1
 #define SOUTH 2
 #define WEST 3
 
-char city[1000][1000];
+char DIRS[4] = { NORTH, EAST, SOUTH, WEST };
+char city[CITY_SIZE][CITY_SIZE];
+
+int TURN_MATRIX[4][2] = {
+  {0, 1}, 
+  {1, 0}, 
+  {0, -1}, 
+  {-1, 0}
+};
 
 int get_next_dir(int current_dir, char move) {
- 
  if (move == 'R') {
-   if (current_dir == NORTH) return EAST;
-   if (current_dir == EAST) return SOUTH;
-   if (current_dir == SOUTH) return WEST;
-   if (current_dir == WEST) return NORTH;
+   return DIRS[(current_dir + 1) % 4];
+ } else {
+   return DIRS[(current_dir + 3) % 4];
  } 
+}
 
- if (move == 'L') {
-   if (current_dir == NORTH) return WEST;
-   if (current_dir == WEST) return SOUTH;
-   if (current_dir == SOUTH) return EAST;
-   if (current_dir == EAST) return NORTH;
- }
+void check_city(int x, int y) {
+  if (city[x][y] > 1) {
+    printf("%d\n", abs(x-START_POS)+abs(y-START_POS));
+    exit(0);
+  }
+}
+
+
+void fill_line(int begin_x, int end_x, int begin_y, int end_y) {
+  int i;
+
+  for(i = begin_x; (end_x > begin_x) ? (i < end_x) : (i > end_x); (end_x > begin_x) ? i++ : i--) {
+      city[i][begin_y] += 1;
+      check_city(i, begin_y);    
+  }
+
+  for(i = begin_y; (end_y > begin_y) ? (i < end_y) : (i > end_y); (end_y > begin_y) ? i++ : i--) {
+      city[begin_x][i] += 1;
+      check_city(begin_x, i);    
+  }
 }
 
 int main(int agrc, char ** argv) {
-
   int current_dir = NORTH;
-  int x = 500, y = 500;
-  int old_x = 500, old_y = 500;
-  int i,j;
+  int x = START_POS, y = START_POS;
+  int ox = START_POS, oy = START_POS;
   char move;
   int distance;
 
   while(scanf("%c%d, ", &move, &distance) == 2) {
     current_dir = get_next_dir(current_dir, move);
-
-    old_x = x;
-    old_y = y;
-
-    if (current_dir == NORTH) y += distance;
-    if (current_dir == SOUTH) y -= distance;
-    if (current_dir == EAST) x += distance;
-    if (current_dir == WEST) x -= distance;
-
-    printf("%d %d - (%d, %d)\n", current_dir, distance, x-500, y-500);
-
-    if (old_x != x) {
-      if (x > old_x) {
-        for(i = old_x; i < x; i++) city[i][y] += 1;
-      } else if (old_x > x) {
-        for(i = x+1; i <= old_x; i++) city[i][y] += 1;
-      }
-    }
     
-    if (old_y != y) {
-      if (y > old_y) {
-        for(i = old_y; i < y; i++) city[x][i] += 1;
-      } else if (old_y > y) {
-        for(i = y+1; i <= old_y; i++) city[x][i] += 1;
-      }
-    }
+    ox = x;
+    oy = y;
 
-    for(i = 0; i < 1000; i++) {
-      for(j = 0; j < 1000; j++) {
-        if (city[i][j] > 1) { printf("->> %d\n", abs(i-500)+abs(j-500)); return 0; }
-      }
-    }
+    x += TURN_MATRIX[current_dir][0] * distance;
+    y += TURN_MATRIX[current_dir][1] * distance;
+
+    fill_line(ox, x, oy, y);
   } 
-  
+
+  printf("%d\n", abs(x-START_POS)+abs(y-START_POS));
+
   return 0;
 }
