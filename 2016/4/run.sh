c73aff4e9456e83b65f7401913d9a687d473f36d
Joao Salavisa <joao.salavisa@gmail.com>
2016-12-04 11:41:19 +0000

    day 4

---
+++ 2016/4/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cat $1 | sed -e 's/-\([0-9]\)/\ \1/g' | sed -e 's/-//g' | sed -E 's/\]//g' | sed -e 's/\[/ /g' | ./p1
+cat $1 | sed -e 's/-\([0-9]\)/\ \1/g' | sed -e 's/-//g' | sed -E 's/\]//g' | sed -e 's/\[/ /g' | ./p2
