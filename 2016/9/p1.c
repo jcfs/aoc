03df8648d64389e7db9d94c7ed4da1f9b14cf6b3
Joao Salavisa <joao.salavisa@gmail.com>
2016-12-09 12:49:36 +0000

    tweaks

--- 2016/9/p1.c
+++ 2016/9/p1.c
@@ -2,32 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 
+char line[64335];
+
 int main(int argc, char ** argv) {
-  char line[64335];
-
+  long total = 0;
+  
   if (fgets(line, 64335, stdin) != NULL) {
-    long total = 0;
-
     char * k = line;
-    char * ok = line;
 
-    // extreme case when there are no brackets at all
-    if (!strchr(k, '(')) 
-      total += strlen(line);
-
-    while((k = strchr(ok, '(')) != NULL) {
-      // sum the amount before the bracket to the total length
-      total += (k - ok);
-
+    for(k = line; *k != '\n'; k++) {
       int r, g;
       if (sscanf(k, "(%dx%d)", &r, &g) == 2) {
         total += r * g;
-        k = strchr(k, ')') + r;    
+        k = strchr(k, ')') + r;
+      } else {
+        total++;
       }
-
-      ok = k;
     }
-
     printf("%ld\n", total);
   } 
 }
