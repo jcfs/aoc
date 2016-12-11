a7219ad6686bf5dcbe99c47d3ebea5ba7a71a8d4
Joao Salavisa <joao.salavisa@gmail.com>
2016-12-11 10:43:43 +0000

    part

---
+++ 2016/11/p1.c
@@ -0,0 +1,164 @@
+/* compile gcc -o p1 p1.c -Wall */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+int floors[4][12] = {
+  { -1,  1,   0,   0,  0,  0, 0, 0, 0, 0, 0, 10},
+  {  0,  0,  -2,  -3, -4, -5, 0, 0, 0, 0, 0, 0},
+  {  0,  0,   0,   0,  0,  0, 2, 3, 4, 5, 0, 0},
+  {  0,  0,   0,   0,  0,  0, 0, 0, 0, 0, 0, 0},
+};
+
+int state[400000][4][12];
+int statesteps[400000] = {[0 ... 200000] = 10000};
+int s = 0;
+
+
+void copy_state(int ele, int steps) {
+  for(int i = 0; i < 4; i++)
+    for(int j = 0; j < 12; j++)
+      state[s][i][j] = floors[i][j];
+
+  state[s][ele][11] = 10;
+  statesteps[s] = steps;
+
+  s++;
+}
+
+char is_prev_state(int steps) {
+  int f = 0;
+  for(int k = 0; k < s; k++) {
+    f = 1;
+    for(int i = 0; i < 4; i++) {
+      for(int j = 0; j < 12; j++)
+        if (state[k][i][j] != floors[i][j]) f = 0;
+    }
+    if (f) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+void print_map(int ele) {
+
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 12; j++) {
+      if (j < 11) printf("% 3d ", floors[i][j]);
+      else if (ele == i) printf(" E ");
+    }
+    printf("\n");
+  }
+  printf("--------------------\n");
+
+}
+
+char is_another_rtg(int n, int chip) {
+  for (int i = 0; i < 4; i++) {
+    if (floors[n][i] < 0 && chip * -1 != floors[n][i]) return 1;
+  }
+
+  return 0;
+}
+
+char own_rtg(int n, int chip) {
+  for (int i = 0; i < 4; i++) {
+    if (floors[n][i] < 0 && chip * -1 == floors[n][i]) return 1;
+  }
+
+  return 0;
+}
+
+char valid_floor(int n) {
+  for(int i = 0; i < 4; i++) {
+    if (floors[n][i] > 0 && is_another_rtg(n, floors[n][i]) && !own_rtg(n, floors[n][i])) return 0;
+  }
+
+  return 1;
+}
+
+char valid_board() {
+  for(int i = 0; i < 4; i++) if (!valid_floor(i)) return 0;
+  return 1;
+}
+
+char is_final() {
+  for(int i = 0; i < 4; i++) if (!floors[3][i]) return 0;
+  return 1;
+}
+
+int min = 10;
+
+void move_up(int ele, int i) {
+  floors[ele+1][i] = floors[ele][i];
+  floors[ele][i] = 0;
+
+  floors[ele+1][11] = 10;
+  floors[ele][11] = 0;
+}
+
+
+void move_down(int ele, int i) {
+  floors[ele-1][i] = floors[ele][i];
+  floors[ele][i] = 0;
+
+  floors[ele-1][11] = 10;
+  floors[ele][11] = 0;
+}
+
+void solve(int ele, int steps) {
+
+  if (steps > min) return;
+
+  copy_state(ele, steps);
+  //print_map(ele);
+  //printf("min: %d curr_steps: %d\n", min, steps);
+
+  if (is_final()) {
+    min = (steps < min) ? steps : min;
+    printf("FINALLLLLLL %d", min);
+    return;
+  }
+
+  for(int i = 0; i < 4; i++) {
+    for(int j = i+1; j < 10; j++) {
+      int a = floors[ele][i];
+      int b = floors[ele][j];
+
+      if (ele < 3) {
+        if (a) move_up(ele, i);
+        if (b) move_up(ele, j);
+        if (valid_board() && (a || b) && (!is_prev_state(steps+1))) {
+          solve(ele + 1, steps + 1);
+        }
+
+        if (a) move_down(ele+1, i);
+        if (b) move_down(ele+1, j);
+
+      }
+
+      if (ele > 0) {
+        if (a) move_down(ele, i);
+        if (b) move_down(ele, j);
+
+        if (valid_board() && (a || b) && (!is_prev_state(steps+1))) {
+          solve(ele - 1, steps+1);
+        }
+
+        if (a) move_up(ele-1, i);
+        if (b) move_up(ele-1, j);
+      }
+    }
+  }
+}
+
+int main(int argc, char ** argv) {
+
+  for(int i = 0; i < 4; i++) {
+    printf("floor %d is %d\n", i + 1, valid_floor(i));
+  }
+
+  solve(0, 0);
+  printf("%d\n", min);
+}
