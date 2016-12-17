f1743e89e2cf94ae7bd1d496aa9acef2b99d849f
Joao Salavisa <joao.salavisa@gmail.com>
2016-12-17 07:17:58 +0000

    day 17

--- 2016/16/p1.c
+++ 2016/16/p1.c
@@ -13,7 +13,7 @@ char * get_checksum(char * input_s, int input_n) {
 
   for(; l < input_n; a[l] = '0', l = l * 2 + 1) 
     for(int i = 0; i < l; i++) 
-      a[l * 2 - i] = (~(a[i] ^ '0') & 1) + '0';
+      a[l * 2 - i] = (~(a[i] ^ '0') & 1);
   // truncate the result to the chars needed
   a[input_n] = 0;
 
