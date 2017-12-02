c8189bb6615aa16370823559f3b03bf925b87744
Joao Salavisa <joao.salavisa@gmail.com>
2017-12-02 11:51:38 +0000

    day 2

---
+++ 2017/02/p1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+  var diff int = 0
+
+  scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		s := scanner.Text()
+    var min, max int = 9000, 0
+
+		for _, n := range strings.Fields(s) {
+			d, _ := strconv.Atoi(n)
+			if d > max {
+				max = d
+			}
+
+			if d < min {
+				min = d
+			}
+		}
+		diff += max - min
+	}
+
+	fmt.Println(diff)
+}
