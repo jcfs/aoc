d10a237c10ea989a3c77d9863e058f8ed057f1cc
Joao Salavisa <joao.salavisa@gmail.com>
2017-12-05 22:15:59 +0000

    2017 day 5

---
+++ 2017/05/p2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var val, pc, steps = 0, 0, 0
+  var s []int
+
+  for {
+    i, _ := fmt.Scanf("%d\n", &val)
+
+    if i == 0 {
+      break
+    }
+    s = append(s, val)
+  }
+
+  for i := 0; i < len(s); i=pc {
+    pc += s[i]
+    steps++
+
+    if (s[i] >= 3) {
+      s[i]--
+    } else {
+      s[i]++
+    }
+
+
+    if (pc < 0 || pc > len(s)) {
+      break
+    }
+  }
+
+  fmt.Println(steps)
+}
