60e50c9af91f6d701ae9df82cd1f4cc46a98ca72
Joao Salavisa <joao.salavisa@gmail.com>
2019-12-08 10:46:50 +0000

    2019 day 8

--- 2019/08/sol.py
+++ 2019/08/sol.py
@@ -2,8 +2,8 @@ import numpy
 
 size = 6 * 25
 
-a = [x for x in list(open('in').readline().replace('0',' ').strip())]
-b = ['2'] * size
+a = [int(x) for x in list(open('in').readline().strip())]
+b = [2] * size
 
 min_z = 10000
 
@@ -11,19 +11,19 @@ for i in range(len(a) // size):
     offset = i * size
     lb, hb = offset, size + offset
 
-    zeros = a[lb:hb].count(' ')
-    ones = a[lb:hb].count('1')
-    twos = a[lb:hb].count('2')
+    zeros = a[lb:hb].count(0)
+    ones = a[lb:hb].count(1)
+    twos = a[lb:hb].count(2)
 
     if zeros < min_z:
        p1 = ones * twos
        min_z = zeros
 
     # for part2 lets run through them all
-    b = [a[y + offset] if b[y] == '2' else b[y] for y in range(size)]
+    b = [a[y + offset] if b[y] == 2 else b[y] for y in range(size)]
 
 print("p1", p1)
 print("p2")
 for i in numpy.array_split(numpy.array(b), 6):
-    print("".join(map(str,i)))
+    print("".join(map(str, numpy.where(i == 0, ' ', i))).replace("1", "#"))
 
