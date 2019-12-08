60a0a4e1864a411fc8e3fefa06da4c08470bb42c
Joao Salavisa <joao.salavisa@gmail.com>
2019-12-08 10:35:59 +0000

    2019 day 8

--- 2019/08/sol.py
+++ 2019/08/sol.py
@@ -1,32 +1,29 @@
-h, w = 6, 25
+import numpy
 
-a = [int(x) for x in list(open('in').readline().strip())]
-b = [2] * (h*w)
+size = 6 * 25
+
+a = [x for x in list(open('in').readline().replace('0',' ').strip())]
+b = ['2'] * size
 
 min_z = 10000
 
-for i in range(int(len(a) / (h*w))):
-    offset = i * h * w
-    lb, hb = offset, h * w + offset
+for i in range(len(a) // size):
+    offset = i * size
+    lb, hb = offset, size + offset
 
-    zeros = a[lb:hb].count(0)
-    ones = a[lb:hb].count(1)
-    twos = a[lb:hb].count(2)
+    zeros = a[lb:hb].count(' ')
+    ones = a[lb:hb].count('1')
+    twos = a[lb:hb].count('2')
 
     if zeros < min_z:
        p1 = ones * twos
        min_z = zeros
 
     # for part2 lets run through them all
-    for y in range(h * w):
-        c = a[y + offset]
-        b[y] = c if b[y] == 2 else b[y]
+    b = [a[y + offset] if b[y] == '2' else b[y] for y in range(size)]
 
 print("p1", p1)
-print("p2", end = '')
-for y in range(h * w):
-    if y % w == 0:
-        print("")
-    print(b[y] if b[y] != 0 else ' ', end = '')
-
+print("p2")
+for i in numpy.array_split(numpy.array(b), 6):
+    print("".join(map(str,i)))
 
